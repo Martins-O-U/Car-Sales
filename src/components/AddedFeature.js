ea3a3af5340f075e7f0cb98995a07096e0090223
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-09 19:53:18 +0100

    App refactored to use redux with reducer as well as clicks

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -4,8 +4,10 @@ const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
-      {props.feature.name}
+      <button className="button" onClick={() => props.removeFeature(props.feature)}>X</button>      
+      <div key={props.feature.id}>
+        {props.feature.name}
+      </div>
     </li>
   );
 };
